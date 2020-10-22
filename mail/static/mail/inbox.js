7a21a19242e0e1744e2162f89d8ee05121c3f845
Caroline Gould <caroline.a.gould@gmail.com>
2020-10-21 21:07:05 -0400

    Test

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
+  // By default, load the inboxx
   load_mailbox('inbox');
 });
 
